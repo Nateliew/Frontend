68e6a81c04d8cb1518510b72b359e50dd8e09c36
Nateliew <weeli.tn@gmail.com>
2023-04-18 13:57:26 +0800

    final

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import List from "@mui/material/List";
 import Grid from "@mui/material/Grid";
-import { useSearchParams, useNavigate } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
 
 export default function App() {
   const [stocks, setStocks] = useState<any[]>([]);
-  let [searchParams, setSearchParams] = useSearchParams();
 
   // load upon starting up the app
   useEffect(() => {
@@ -37,6 +36,7 @@ export default function App() {
       .then(
         axios.spread((...responses) => {
           //map out and check if there are duplicates
+          let newStocks: any = [];
           responses.map((response: any) => {
             let name = response.data[0].companyName;
             let check = stocks.some((el) => el.companyName === name);
@@ -46,118 +46,77 @@ export default function App() {
               stocks.push(response.data[0]);
             }
           });
+          setStocks([...stocks, ...newStocks]);
         })
       );
   };
 
   const handleSave = async (stocks: any) => {
-    console.log("work", stocks);
-    // await stocks.forEach((element: any) => {
-    //   axios.post(`${process.env.REACT_APP_API_SERVER}`, {
-    //     data: {
-    //       symbol: element.symbol,
-    //       name: element.companyName,
-    //       price: element.latestPrice,
-    //       percent: element.changePercent,
-    //       change: element.change,
-    //       image: `https://storage.googleapis.com/iex/api/logos/${element.symbol}.png`,
-    //     },
-    //   });
-    // });
+    await stocks.forEach((element: any) => {
+      axios.post(`${process.env.REACT_APP_API_SERVER}`, {
+        data: {
+          symbol: element.symbol,
+          name: element.companyName,
+          price: element.latestPrice,
+          percent: element.changePercent,
+          change: element.change,
+          image: `https://storage.googleapis.com/iex/api/logos/${element.symbol}.png`,
+        },
+      });
+    });
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Stock price</h3>
-        <h6>stocks</h6>
-        <div className="search">
-          <p className="searchButton">Search for your trip: </p>
-          <input
-            value={searchParams.get("filter") || ""}
-            onChange={(event) => {
-              let filter = event.target.value;
-              if (filter) {
-                setSearchParams({ filter });
-              } else {
-                setSearchParams({});
-              }
-            }}
-          />
-        </div>
+        <h3>Pints Coding Task</h3>
+        <h6>Stocks Price</h6>
+
         <br />
         <div>
           <div className="stocks-container">
             <List sx={{ width: 550, bgcolor: "background.paper" }}>
-              {stocks
-                .filter((stock) => {
-                  let filter = searchParams.get("filter");
-                  if (!filter) return true;
-                  let name = stock.companyName;
-                  return name === filter;
-                })
-                .map((stock: any) => (
-                  <div className="stock" key={stock.id}>
-                    <img
-                      src={`https://storage.googleapis.com/iex/api/logos/${
-                        stock.symbol as string
-                      }.png`}
-                      alt="logo"
-                      className="pic"
-                    />
-                    <div className="middle">
-                      <h4>{stock.symbol}</h4>
-                      <h6 className="middleC">{stock.companyName}</h6>
-                    </div>
-                    <div className="right">
-                      <Grid
-                        container
-                        rowSpacing={1}
-                        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                      >
-                        <Grid item xs={12}>
-                          <h5>${stock.latestPrice}</h5>
-                        </Grid>
-                        <Grid item xs={4} container spacing={1}>
-                          <h6
-                            className={stock.changePercent > 0 ? "good" : "bad"}
-                          >
-                            {stock.change}
-                          </h6>
-                        </Grid>
-                        <Grid item xs={4}>
-                          <h6
-                            className={
-                              stock.changePercent > 0 ? "good1" : "bad1"
-                            }
-                          >
-                            {(stock.changePercent * 100).toFixed(2)}%
-                          </h6>
-                        </Grid>
-                      </Grid>
-                    </div>
-
-                    {/* <div className="right">
-                    <h5>${stock.latestPrice}</h5>
-                  <div className="right1">
-                    <h6 className={stock.changePercent > 0 ? "good" : "bad"}>
-                      {stock.changePercent}
-                    </h6>
-                    </div>
-                                      <div className="right2">
-
-                    <h6 className={stock.changePercent > 0 ? "good" : "bad"}>
-                      {stock.change}
-                    </h6>
-                    </div> */}
-                    {/* </div> */}
-                    {/* style a divider here */}
-                    <hr className="single" />
+              {stocks.map((stock: any) => (
+                <div className="stock" key={stock.id}>
+                  <img
+                    src={`https://storage.googleapis.com/iex/api/logos/${
+                      stock.symbol as string
+                    }.png`}
+                    alt="logo"
+                    className="pic"
+                  />
+                  <div className="middle">
+                    <h4>{stock.symbol}</h4>
+                    <h6 className="middleC">{stock.companyName}</h6>
                   </div>
-                ))}
+                  <div className="right">
+                    <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                      <Grid item xs={12}>
+                        <h5>${stock.latestPrice}</h5>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <h6
+                          className={stock.changePercent > 0 ? "good" : "bad"}
+                        >
+                          {stock.change}
+                        </h6>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <h6
+                          className={stock.changePercent > 0 ? "good1" : "bad1"}
+                        >
+                          {(stock.changePercent * 100).toFixed(2)}%
+                        </h6>
+                      </Grid>
+                    </Grid>
+                  </div>
+                </div>
+              ))}
             </List>
           </div>
-          <button onClick={() => handleSave(stocks)}>Save</button>
+          <button className="btn-grad" onClick={() => handleSave(stocks)}>
+            Save your stock list
+          </button>
         </div>
       </header>
     </div>
